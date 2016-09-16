327023eea8dbf8b3d971b85fa131ec5c80dc032f
ChDF T <chdft-dev@outlook.de>
2016-09-16 20:46:53 +0200

    add single assembly source

---
+++ ExtensionBridge/ExtensionBridge/SingleAssemblySource.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionBridge
+{
+	/// <summary>
+	/// Uses a provided assembly as source for extensions.
+	/// </summary>
+	public class SingleAssemblySource : ISource
+	{
+		public SingleAssemblySource(Assembly assembly)
+		{
+			Assembly = assembly;
+		}
+
+		public Assembly Assembly { get; private set; }
+
+		public IEnumerable<Assembly> GetAssemblies()
+		{
+			return new Assembly[] { Assembly };
+		}
+	}
+}
